6467e435b048bd610d8b4709f8ae3366290aa4a6
JP Lehr <JanPatrick.Lehr@amd.com>
2022-12-22 09:47:06 -0500

    Updates test

--- test/smoke-fails/ompt-async-events-parfor-array/async-events.cpp
+++ test/smoke-fails/ompt-async-events-parfor-array/async-events.cpp
@@ -13,34 +13,38 @@ int identity(int i) { return i; }
 /// @param argv
 /// @return
 int main(int argc, char **argv) {
-  const int threadParallel = 4;
-  const int deviceStress = 1;
+  const int threadParallel = 1;
+  const int deviceComputeLoad = 1;
 
-// We should see 3 * threadParallel * deviceStress tracing events
+// For threadParallel == 1 && deviceComputeLoad == 1 -> 9 Events observed
 #pragma omp parallel for schedule(static,1)
   for (int i = 0; i < threadParallel; ++i) {
-    int vals[deviceStress] = {0};
-    int vals2[deviceStress] = {0};
-    int vals3[deviceStress] = {0};
+    int vals[deviceComputeLoad] = {0};
+    int vals2[deviceComputeLoad] = {0};
+    int vals3[deviceComputeLoad] = {0};
 
+// Generates 2 data-in events, 2 data-out events
 #pragma omp target data map(vals,vals2)
 {
+  // Generates 1 kernel launch event
 #pragma omp target teams distribute nowait
-    for (int j = 0; j < deviceStress; ++j) {
+    for (int j = 0; j < deviceComputeLoad; ++j) {
       vals[j] += 1;
     }
+// Generates 1 kernel launch event
 #pragma omp target teams distribute nowait
-    for (int j = 0; j < deviceStress; ++j) {
+    for (int j = 0; j < deviceComputeLoad; ++j) {
       vals2[j] += 1;
     }
 }
 
+// Generates 1 data-in event, 1 data-out event, 1 kernel launch event
 #pragma omp target teams distribute map(vals3)
-    for(int j = 0; j< deviceStress; ++j) {
+    for(int j = 0; j< deviceComputeLoad; ++j) {
       vals3[j] += 2;
     }
 
-    for (int j = 0; j < deviceStress; ++j) {
+    for (int j = 0; j < deviceComputeLoad; ++j) {
       if (vals3[j] != 2) {
         std::cout << "MISMATCH " << vals3[j] << std::endl;
         abort();
