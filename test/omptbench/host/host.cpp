08d9979f207eb331599abc5222ccff914b794c97
JP Lehr <JanPatrick.Lehr@amd.com>
2024-07-30 06:10:08 -0500

    [HPCBugsFest] Adding some more host tests

--- test/omptbench/host/host.cpp
+++ test/omptbench/host/host.cpp
@@ -14,7 +14,7 @@ TEST(WorkDistribution, SingleParallelFor) {
 
   int N = 100000;
   int i;
-  int numThreads=omp_get_num_threads();
+  int numThreads=4;
 
   int a[N];
   int b[N];
@@ -51,8 +51,9 @@ TEST(WorkDistribution, SingleParallelForNumThreadsClause) {
   for (i=0; i<N; i++)
     b[i]=i;
 
-  OMPT_ASSERT_SET(ParallelBegin, numThreads);
-  OMPT_ASSERT_SET(ThreadBegin, ompt_thread_worker);
+  OMPT_ASSERT_SEQUENCE(ParallelBegin, numThreads);
+  OMPT_ASSERT_SEQUENCE_SUSPEND();
+  OMPT_ASSERT_SEQUENCE(ThreadBegin, ompt_thread_worker);
 #pragma omp parallel for num_threads(numThreads)
   {
     for (int j = 0; j< N; j++)
@@ -114,6 +115,57 @@ TEST(WorkDistribution, DISABLED_TwoNestedParallelForNumThreadsClause) {
     }
   }
 }
+
+TEST(WorkDistribution, SingleParallelForWithSingleSection){
+  int N = 100000;
+  int i, c;
+
+  int a[N];
+  int b[N];
+
+  for (i=0; i<N; i++)
+    a[i]=0;
+
+  for (i=0; i<N; i++)
+    b[i]=i;
+
+#pragma omp parallel
+  {
+      #pragma omp single
+      {
+        c = 666;
+      }
+    #pragma omp for
+    for (int j = 0; j< N; j++) {
+      a[j] = b[j];
+    }
+  }
+}
+
+TEST(WorkDistribution, SingleParallelWithSingleSection) {
+  // TODO: Implement
+}
+
+TEST(WorkDistribution, SingleParallelWithScopeSection) {
+  // TODO: Implement
+}
+
+TEST(WorkDistribution, SingleParallelOneSectionsTwoSection) {
+  // TODO: Implement
+}
+
+TEST(WorkDistribution, SingleParallelForStaticSchedule) {
+  // TODO: Implement
+}
+
+TEST(WorkDistribution, SingleParallelForDynamicSchedule) {
+  // TODO: Implement
+}
+
+TEST(WorkDistribution, SingleParallelForGuidedSchedule) {
+  // TODO: Implement
+}
+
 
 int main(int argc, char **argv) {
   Runner R;
